09e2bf170db45b1734a31c9f509855163e0904a4
Andrew DeMarco <andrewd1137@gmail.com>
2026-04-06 23:51:05 -0400

    updated some methods and started adding background color changes

--- ViewModels/HomePageViewModel.cs
+++ ViewModels/HomePageViewModel.cs
@@ -1,3 +1,4 @@
+using ColorThiefDotNet;
 using Models.NASAOpenAPI;
 using Services;
 
@@ -11,6 +12,8 @@ namespace ViewModels
 
         public Root Expedition40SpaceMissions { get; set; } = new Root();
 
+        public string ImagePath { get; set; } = "";
+
         public HomePageViewModel(INASAOpenAPIService nasaOpenAPIService)
         {
             this._NASAOpenAPIService = nasaOpenAPIService;
@@ -19,8 +22,36 @@ namespace ViewModels
         public async Task LoadAsync()
         {
             this.IsLoading = true;
+            
+            //todo: cache this result so we don't have to make a new API call every time the user visits the home page
             this.Expedition40SpaceMissions = await this._NASAOpenAPIService.GETExpedition40SpaceMissions();
+            this.ImagePath = this.DisplayRandomExpidition40Image();
             this.IsLoading = false;
+        }
+
+        public string DisplayRandomExpidition40Image()
+        {
+            var random = new Random();
+            var randomIndex = random.Next(0, this.Expedition40SpaceMissions.Collection.Items.Count);
+            var randomImageUrl = this.Expedition40SpaceMissions.Collection.Items[randomIndex].Links[0].Href;
+            return randomImageUrl;
+        }
+
+        public void DeterminePallete()
+        {
+            var colorThief = new ColorThief();
+
+            using (var bitmap = new System.Drawing.Bitmap(this.ImagePath))
+            {
+                var palette = colorThief.GetPalette(bitmap, 8);
+
+                foreach (var quantizedColor in palette)
+                {
+                    var color = quantizedColor.Color;
+                    Console.WriteLine($"RGB: {color.R}, {color.G}, {color.B} | Population: {quantizedColor.Population}");
+                    //todo make this output the colors to be used in the homt page design (background color, text color, accent color, etc.)
+                }
+            }    
         }
     }
 }
